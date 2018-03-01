0ba5174954c408ae632c17b06a223a540d578718
Papu <papu559999@gmail.com>
2018-03-01 16:00:03 +0530

    First change. Added a simple output in MessageSender

--- MessageTools/src/com/javadot/beans/MessageSender.java
+++ MessageTools/src/com/javadot/beans/MessageSender.java
@@ -4,7 +4,7 @@ public class MessageSender {
 	
 	public void sendMessage(String name, String message) {
 		
-		
+		System.out.println("Hello "+name+" , your message is in Que... ");
 		System.out.println("Hello Mr/Mrs : "+name+" ,Your message is "
 						+message+" is sending...");
 	}
