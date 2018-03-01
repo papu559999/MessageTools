8d00af760d82f0063e222a5d9671b6f51a84740c
Papu <papu559999@gmail.com>
2018-03-01 15:45:17 +0530

    Initial check in of MessageTools

---
+++ MessageTools/src/com/javadot/beans/MessageSender.java
@@ -0,0 +1,12 @@
+package com.javadot.beans;
+
+public class MessageSender {
+	
+	public void sendMessage(String name, String message) {
+		
+		
+		System.out.println("Hello Mr/Mrs : "+name+" ,Your message is "
+						+message+" is sending...");
+	}
+
+}
---
+++ MessageTools/src/com/javadot/test/MessageTest.java
@@ -0,0 +1,11 @@
+package com.javadot.test;
+
+import com.javadot.beans.MessageSender;
+
+public class MessageTest {
+	public static void main(String[] args) {
+		MessageSender ms = new MessageSender();
+		ms.sendMessage("Sriman", "Heppy holly.......");
+	}
+
+}
